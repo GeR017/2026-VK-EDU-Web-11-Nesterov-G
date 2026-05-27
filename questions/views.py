e531a7b48fbaaff07e79c8ae09a075ecea9a6d05
GeR017 <122767280+GeR017@users.noreply.github.com>
2026-05-28 02:53:43 +0300

    Add files via upload

--- questions/views.py
+++ questions/views.py
@@ -1,9 +1,13 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 
+from .forms import AskForm, AnswerForm
 from .models import Question, Answer, Tag
+from django.urls import reverse, reverse_lazy
 
+@login_required(login_url=reverse_lazy('core:login'))
 def index(request):
 
     questions_list = Question.objects.new().prefetch_related("tags")
@@ -31,15 +35,35 @@ def question(request, question_id):
     item = get_object_or_404(Question.objects.get_detailed(question_id))
     answer_list = Answer.objects.for_question(question_id=question_id)
 
-    paginator = Paginator(answer_list, 10)
-    page_num = request.GET.get('page', 1)
-    page_obj = paginator.get_page(page_num)
 
-    return render(request, 'questions/question.html', {
-        'question': item,
-        'answers': page_obj,
-        'page_obj': page_obj,
-    })
+    if request.method == 'POST':
+        if not request.user.is_authenticated:
+            return HttpResponseRedirect(reverse('login'))
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.question = item
+            answer.author = request.user
+            answer.save()
+
+            base_url = reverse('question', args=[question_id])
+
+            last_page = (answer_list.count() + 9)//10
+
+            return HttpResponseRedirect(f"{base_url}?page={last_page}#answer-{answer.id}")
+
+    else:
+        form = AnswerForm()
+        paginator = Paginator(answer_list, 10)
+        page_num = request.GET.get('page', 1)
+        page_obj = paginator.get_page(page_num)
+
+        return render(request, 'questions/question.html', {
+            'question': item,
+            'answers': page_obj,
+            'page_obj': page_obj,
+            'form' : form,
+        })
 
 
 def tag(request, tag_name):
@@ -56,6 +80,19 @@ def tag(request, tag_name):
         'page_obj': page_obj
     })
 
-#@login_required
+@login_required(login_url='login')
 def ask(request):
-    return render(request, 'questions/ask.html')
+    if request.method == "POST":
+        form = AskForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+
+            question.author = request.user
+            question.save()
+
+            form.save_m2m()
+            return HttpResponseRedirect(question, id=question.id)
+    else:
+        form = AskForm()
+    return render(request, 'questions/ask.html', {'form' : form})
+
